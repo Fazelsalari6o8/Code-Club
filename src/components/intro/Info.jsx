8409d6a33969512455132ce6a072b9d8928c1193
Anonymous Soldier <a96111085154015@gmail.com>
2024-07-10 13:17:46 +0330

    Implementation of intro section with Tailwind

--- src/components/intro/Info.jsx
+++ src/components/intro/Info.jsx
@@ -1,56 +1,28 @@
-// mui
-import { Box, Grid, Typography } from "@mui/material";
-
 function Info() {
   return (
-    <Box component="div" display={"flex"} justifyContent={"center"}>
-      <Grid
-        container
-        xs={12}
-        md={10}
-        lg={8}
-        sx={{
-          backgroundColor: "rgba(255, 255, 255, 0.1)",
-          backdropFilter: "blur(30px)",
-          borderRadius: "16px",
-          padding: "32px",
-          marginTop: { xs: "10px", sm: "0" },
-        }}
-      >
-        <Grid item xs={6} sm={12} mb={4} textAlign={"center"}>
-          <Typography component="p" variant="p" color="primary.200">
-            ۲۲۰
-          </Typography>
-          <Typography component="p" variant="p" color="primary.400">
-            مدرس
-          </Typography>
-        </Grid>
-        <Grid item xs={6} sm={12} mb={4} textAlign={"center"}>
-          <Typography component="p" variant="p" color="primary.200">
-            ۱۲,۵۲۴
-          </Typography>
-          <Typography component="p" variant="p" color="primary.400">
-            دانشجو
-          </Typography>
-        </Grid>
-        <Grid item xs={6} sm={12} mb={{ xs: "0", sm: 4 }} textAlign={"center"}>
-          <Typography component="p" variant="p" color="primary.200">
-            ۳۶۰,۰۰۰
-          </Typography>
-          <Typography component="p" variant="p" color="primary.400">
-            ساعت آموزش
-          </Typography>
-        </Grid>
-        <Grid item xs={6} sm={12} textAlign={"center"}>
-          <Typography component="p" variant="p" color="primary.200">
-            ۳۶۰,۰۰۰
-          </Typography>
-          <Typography component="p" variant="p" color="primary.400">
-            دوره آموزشی
-          </Typography>
-        </Grid>
-      </Grid>
-    </Box>
+    <div className="flex justify-center">
+      <div className="grid grid-cols-12 w-[302px] bg-[#ffffff19] backdrop-blur-[30px] rounded-2xl py-8 px-6 mt-3 sm:w-36 sm:p-8 sm:mt-0 md:grid-cols-10 lg:grid-cols-8">
+        <div className="h-[54px] w-[79px] col-start-1 col-end-6 mb-8 text-center sm:col-start-1 sm:col-end-13">
+          <p className="text-primary-200">۲۲۰</p>
+          <p className="text-primary-400">مدرس</p>
+        </div>
+
+        <div className="h-[54px] w-[79px] col-start-8 col-end-13 mb-8 text-center sm:col-start-1 sm:col-end-13">
+          <p className="text-primary-200">۱۲,۵۲۴</p>
+          <p className="text-primary-400">دانشجو</p>
+        </div>
+
+        <div className="h-[54px] w-[79px] col-start-1 col-end-6 text-center sm:mb-8 sm:col-start-1 sm:col-end-13">
+          <p className="text-primary-200">۳۰,۰۰۰</p>
+          <p className="text-primary-400 text-nowrap">ساعت آموزش</p>
+        </div>
+
+        <div className="h-[54px] w-[79px] col-start-8 col-end-13 text-center sm:col-start-1 sm:col-end-13">
+          <p className="text-primary-200">۳۶۰,۰۰۰</p>
+          <p className="text-primary-400 text-nowrap">دوره آموزشی</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
